e73915b6d6fd1c27938282299d1efea73087f008
realJohnLK <johnluke@live.de>
2024-10-28 18:45:47 +0100

    SeriLog wurde hinzugefügt, für besseres Loggen. Zudem wird ein Log im Logs Ordner erstellt

--- TicketAPI/Program.cs
+++ TicketAPI/Program.cs
@@ -1,6 +1,13 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+// SeriLog
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(builder.Configuration));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
