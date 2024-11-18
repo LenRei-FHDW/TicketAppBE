a61921ee452769d0c505164afdcab4613a3e43e7
Sven Kenning <skenning@dspace.de>
2024-11-18 14:37:41 +0100

    First comments

--- TicketAPI/Controllers/AuthController.cs
+++ TicketAPI/Controllers/AuthController.cs
@@ -4,7 +4,9 @@ using TicketAPI.Services.DTO;
 
 namespace TicketAPI.Controllers
 {
-    
+    /// <summary>
+    /// This controller manages all authentification api calls.
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
@@ -16,6 +18,11 @@ namespace TicketAPI.Controllers
             _authService = authService;
         }
         
+        /// <summary>
+        /// Response to a login call.
+        /// </summary>
+        /// <param name="model">Email and password of the login.</param>
+        /// <returns>A token if successfull.</returns>
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginModelDTO model)
         {
@@ -34,6 +41,11 @@ namespace TicketAPI.Controllers
             return Ok(new { token = result.Token });
         }
 
+        /// <summary>
+        /// Response to a register call.
+        /// </summary>
+        /// <param name="model">Full name, email and password used to register.</param>
+        /// <returns>The result of the request.</returns>
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterModelDTO model)
         {
@@ -51,9 +63,15 @@ namespace TicketAPI.Controllers
             return BadRequest();
         }
 
+        /// <summary>
+        /// Removes a user.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPost("delete-user")]
         public async Task<IActionResult> DeleteUser([FromBody] ResendConfirmationEmailModelDTO model)
         {
+            // #TODO Secure the deletion
             await _authService.DeleteUser(model);
             return NoContent();
         }
