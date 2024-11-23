26c56f999e91d1b89dc8caf5585ec1957e9a4008
realJohnLK <johnluke@live.de>
2024-11-23 20:03:13 +0100

    Fix ControllerName

--- TicketAPI/Constants/Constants.cs
+++ TicketAPI/Constants/Constants.cs
@@ -4,7 +4,7 @@ namespace TicketAPI.Constants;
 
 public static class Constants
 {
-    public const string ConfirmEmailController = nameof(EmailController.ConfirmEmail);
+    public const string ConfirmEmailController = nameof(AuthController.ConfirmEmail);
     public const string ResetPasswordPath = "reset-password";
     public const string FrontendUrl = "localhost:0000/";
 }
