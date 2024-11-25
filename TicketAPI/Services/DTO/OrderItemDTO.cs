099967e2eac834f5f126e8605063a810c2a8d655
realJohnLK <johnluke@live.de>
2024-11-25 15:21:17 +0100

    Add OrderItemId

--- TicketAPI/Services/DTO/OrderItemDTO.cs
+++ TicketAPI/Services/DTO/OrderItemDTO.cs
@@ -2,6 +2,7 @@
 
 public class OrderItemDTO
 {
+    public Guid OrderItemId { get; set; }
     public Guid ProductId { get; set; }
     public string Name { get; set; } = string.Empty;
     public int Quantity { get; set; }
